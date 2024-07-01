46183ac99d0842689208acb23b1905f954f167a5
Leo Zheng <leozheng@Leos-Laptop.lan>
2024-07-01 15:22:15 -0700

    added the header component

---
+++ react-project/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import cafeLogo from '../../public/images/LogoW.png'
+import '../../styles/templates/header.css'
+
+export default function Header() {
+    return (
+        <nav class="header">
+            <div class="logo-container">
+                <a href="../../index.html">
+                    <img src={cafeLogo} alt="cafe logo" id="cafe-logo"/>
+                </a>
+                <small>Experience Sustainability</small>
+            </div>
+            <div id="nav-pages">
+                <a>Our Specialty</a>
+                <a>About Us</a>
+                <a href="../../menu.html">Menu</a>
+                <a>Careers</a>
+                <div id="order-now-button">
+                    <a>Order Now</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ react-project/src/menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Header from './components/Header'
+import '../styles/menu.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Header />
+    </React.StrictMode>,
+)

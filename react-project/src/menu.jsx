dfef077e6c6cad1a014b71b7575a2d936e21b53e
Leo Zheng <leozheng@Leos-Laptop.lan>
2024-07-01 15:25:37 -0700

    Refactored directories from '../../x' to '/x'

--- react-project/src/menu.jsx
+++ react-project/src/menu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header'
-import '../styles/menu.css'
+import '/styles/menu.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
